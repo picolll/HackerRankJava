52b1f9cd1099d17932e18aef35a2cc1116bebd73
Piotr Lipski <piotr.lipski@bbc.co.uk>
2015-11-02 14:18:59 +0000

    Add Hello World class

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+/**
+ * Created by lipskp01 on 02/11/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!2");
+        System.out.println("Hello world!3");
+
+    }
+
+}
